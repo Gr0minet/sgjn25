314827ad6d3cceb9aea6260a29fb60a9b0e0d5d3
Clément Rivaille <rivaille.clement@orange.fr>
2025-04-26 10:15:53 +0200

    feat: moving block

---
+++ scripts/moving_block.gd
@@ -0,0 +1,30 @@
+extends Area2D
+class_name MovingBlock
+
+@export var color_neutral: Color = Color.WHITE
+@export var color_invalid: Color
+@export var color_valid: Color
+
+@onready var inside_area: Area2D = $InsideArea
+
+var opacity: float
+var is_connected := false
+var is_overlapping := false
+
+func _process(delta: float) -> void:
+	var cursor_position := get_viewport().get_camera_2d().get_global_mouse_position()
+	global_position = cursor_position
+	opacity = modulate.a
+	color_neutral = Color(color_neutral, opacity)
+	color_invalid = Color(color_invalid, opacity)
+	color_valid = Color(color_valid, opacity)
+	
+	is_connected = has_overlapping_areas()
+	is_overlapping = inside_area.has_overlapping_bodies()
+	
+	if is_overlapping:
+		modulate = color_invalid
+	elif is_connected:
+		modulate = color_valid
+	else:
+		modulate = color_neutral
