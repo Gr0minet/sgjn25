9675955f653f52f22f7b29bd30edefd56dac7e77
Clément Rivaille <rivaille.clement@orange.fr>
2025-04-26 15:17:02 +0200

    feat(block): color on instantiate

--- scripts/blocks/block.gd
+++ scripts/blocks/block.gd
@@ -2,11 +2,15 @@ extends PhysicsBody2D
 class_name Block
 
 @onready var shape: Shape2D = $CollisionShape2D.shape
+@onready var sprite: Sprite2D = $Sprite2D
+
+@export var color: Color = Color.BLACK
+@export var is_static := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	if !is_static:
+		sprite.self_modulate = color
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
