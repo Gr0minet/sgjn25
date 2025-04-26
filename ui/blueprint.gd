212f7f0d07ea8d9390fa43957c06452ccf36ab2a
Gr0minet <not@existing.abcdef>
2025-04-26 10:53:48 +0200

    Ajout scene blueprint

--- ui/blueprint.gd
+++ ui/blueprint.gd
@@ -1,3 +1,4 @@
+@tool
 class_name Blueprint
 extends VBoxContainer
 
@@ -5,6 +6,14 @@ extends VBoxContainer
 signal clicked(moving_block_scene: PackedScene)
 
 @export var moving_block_scene: PackedScene = preload("uid://5011xni60ds6")
+@export var price: int = 100: # euro
+	set(value):
+		if not is_node_ready():
+			await ready
+		price = value
+		_label.text = str(price) + "€"	
+
+@onready var _label: Label = $Label
 
 
 func _gui_input(event: InputEvent) -> void:
